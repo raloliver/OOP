ac34a2d83a616a0f1d1a67da7bcb605785fa3ed4
raloliver <raloliver.ws@gmail.com>
2023-08-29 14:19:20 +0100

    add files to project

---
+++ OPP/Item.cs
@@ -0,0 +1,19 @@
+﻿class Product {
+    public string name;
+    public string info;
+    public int quantity;
+    public bool premium;
+
+
+    public void ShowProductDetails()
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Info: {info}");
+        Console.WriteLine($"Quantity: {quantity}");
+
+        if (premium)
+        {
+            Console.WriteLine($"It's only avaliable for members!");
+        }
+    }
+}
---
+++ OPP/Program.cs
@@ -0,0 +1,13 @@
+﻿Product product = new Product();
+product.name = "Product name";
+product.info = "Product info";
+product.quantity = 100;
+product.premium = true;
+
+Product product1 = new Product();
+product1.name = "Product name 1";
+product1.info = "Product info 1";
+product1.quantity = 0;
+
+product.ShowProductDetails();
+product1.ShowProductDetails();
